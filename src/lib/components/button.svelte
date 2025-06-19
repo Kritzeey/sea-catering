8b97953f981f568d44428a70aae4b42fa562f0f6
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-06-19 16:33:10 +0700

    feat: add button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { cva } from "class-variance-authority";
+
+  const button = cva("cursor-pointer h-12 px-6 rounded-3xl", {
+    variants: {
+      variant: {
+        primary: "bg-primary text-white",
+        secondary: "border border-4 border-primary text-primary",
+      },
+    },
+  });
+
+  interface Props {
+    children?: Snippet;
+    variant?: "primary" | "secondary";
+    type?: "button" | "submit" | "reset" | null | undefined;
+  }
+
+  let { children, variant = "primary", type }: Props = $props();
+</script>
+
+<button {type} class={button({ variant })}>
+  {@render children?.()}
+</button>
--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,16 +1,25 @@
+<script lang="ts">
+  import Button from "./button.svelte";
+</script>
+
 <div class="flex h-dvh w-full items-center justify-center">
   <div class="flex flex-col gap-8">
     <div class="flex flex-col gap-4">
-      <span class="text-7xl font-bold text-primary">SEA Catering</span>
-      <span class="font-medium text-xl">Healthy Meals, Anytime, Anywhere</span>
+      <span class="text-primary text-7xl font-bold">SEA Catering</span>
+      <span class="text-xl font-medium">Healthy Meals, Anytime, Anywhere</span>
     </div>
     <div class="flex flex-col">
-      <span>
-        Customizable, healthy, and hassle-free meal service.
-      </span>
-      <span>
-        Delivering all across Indonesia.
-      </span>
+      <span> Customizable, healthy, and hassle-free meal service. </span>
+      <span> Delivering all across Indonesia. </span>
+    </div>
+    <div class="flex gap-4">
+      <Button variant="primary">
+        <span class="font-bold"> Get Started </span>
+      </Button>
+
+      <Button variant="secondary">
+        <span class="font-bold"> Our Menu </span>
+      </Button>
     </div>
   </div>
 </div>
