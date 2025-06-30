db2dea3db3a64131f35bc969e4f887cf2951965c
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-06-30 10:59:30 +0700

    feat: add signup page

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -3,6 +3,7 @@
   import { Eye, EyeClosed, Lock, User } from "@lucide/svelte";
 
   let showPassword = $state(false);
+  let showConfirmPassword = $state(false);
 </script>
 
 <div class="flex h-dvh items-center justify-center">
@@ -10,7 +11,7 @@
     class="mx-auto flex w-full max-w-xl flex-col gap-8 rounded-2xl bg-white p-8 shadow-xs"
   >
     <span class="text-primary w-full text-center text-4xl font-bold">
-      Welcome Back!
+      Create an Account
     </span>
     <div class="flex w-full flex-col gap-4">
       <label for="username"> Username </label>
@@ -47,13 +48,36 @@
         {/if}
       </div>
     </div>
+    <div class="flex w-full flex-col gap-4">
+      <label for="confirmpassword">Confirm Password </label>
+      <div class="relative w-full">
+        <input
+          type={showPassword ? "text" : "password"}
+          id="confirmpassword"
+          placeholder="Confirm Password..."
+          class="h-12 w-full rounded-3xl border p-4 px-12"
+        />
+        <Lock class="absolute top-[25%] left-4" />
+        {#if showConfirmPassword}
+          <Eye
+            onclick={() => (showConfirmPassword = false)}
+            class="absolute top-[25%] right-4 cursor-pointer hover:opacity-60"
+          />
+        {:else}
+          <EyeClosed
+            onclick={() => (showConfirmPassword = true)}
+            class="absolute top-[25%] right-4 cursor-pointer hover:opacity-60"
+          />
+        {/if}
+      </div>
+    </div>
     <div class="flex w-full justify-center">
       <Button>Log in</Button>
     </div>
     <div class="flex w-full justify-center">
-      Don&apos;t have an account?&nbsp;<a
-        href="/signup"
-        class="underline duration-200 hover:opacity-60">Sign up</a
+      Already have an account?&nbsp;<a
+        href="/login"
+        class="underline duration-200 hover:opacity-60">Log in</a
       >
     </div>
   </form>
