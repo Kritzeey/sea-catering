14d8530123db0130486e2c21cdcb944463f6a683
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-06-30 11:07:02 +0700

    fix: change several signup fields

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import Button from "$lib/components/button.svelte";
-  import { Eye, EyeClosed, Lock, User } from "@lucide/svelte";
+  import { AtSign, Eye, EyeClosed, Lock, User } from "@lucide/svelte";
 
   let showPassword = $state(false);
   let showConfirmPassword = $state(false);
@@ -13,16 +13,42 @@
     <span class="text-primary w-full text-center text-4xl font-bold">
       Create an Account
     </span>
+    <div class="grid grid-cols-2 gap-4">
+      <div class="col-span-1 flex flex-col gap-4">
+        <label for="firstname"> First Name </label>
+        <div class="relative w-full">
+          <input
+            type="text"
+            id="firstname"
+            placeholder="First Name..."
+            class="h-12 w-full rounded-3xl border p-4 px-12"
+          />
+          <User class="absolute top-[25%] left-4" />
+        </div>
+      </div>
+      <div class="col-span-1 flex flex-col gap-4">
+        <label for="lastname"> Last Name </label>
+        <div class="relative w-full">
+          <input
+            type="text"
+            id="lastname"
+            placeholder="Last Name..."
+            class="h-12 w-full rounded-3xl border p-4 px-12"
+          />
+          <User class="absolute top-[25%] left-4" />
+        </div>
+      </div>
+    </div>
     <div class="flex w-full flex-col gap-4">
-      <label for="username"> Username </label>
+      <label for="email"> Email </label>
       <div class="relative w-full">
         <input
-          type="text"
-          id="username"
-          placeholder="Username..."
+          type="email"
+          id="email"
+          placeholder="Email..."
           class="h-12 w-full rounded-3xl border p-4 px-12"
         />
-        <User class="absolute top-[25%] left-4" />
+        <AtSign class="absolute top-[25%] left-4" />
       </div>
     </div>
     <div class="flex w-full flex-col gap-4">
